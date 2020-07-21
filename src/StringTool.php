8c488913a3e7bf6611f142889af30f77daa17025
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-07-21 19:19:59 -0300

    Eliminated deprecated functions

--- src/StringTool.php
+++ src/StringTool.php
@@ -1,39 +1,9 @@
 <?php
 
 namespace Plinct\Tool;
-/*
- * 
- */
+
 class StringTool 
-{    
-   /* public static function attributesOrderInArray(array $attributes)
-    {
-        $return = array();
-        foreach ($attributes as $value) {
-            $return[$value['name']]= $value['value'];
-        }
-        return $return;
-    }
-    
-    // ORGANIZA ARRAY POST
-    public static function arrayPostForForm()
-    {
-        $POST = filter_input_array(INPUT_POST);
-        unset($POST['submit']);
-        unset($POST['submit_x']);
-        unset($POST['submit_y']);
-        unset($POST['MAX_FILE_SIZE']);
-        return $POST;
-    }
-    
-    // include path
-    public static function includePath($path)
-    {
-        if(strpos($path, get_include_path()) === false){
-            set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-        }
-    }*/
-        
+{            
     /**
      * REMOVE ACENTOS E ESPAÇOS EM TEXTO
      * @param string $string
@@ -47,17 +17,4 @@ class StringTool
         
         return preg_replace("/[^\.\/a-zA-Z0-9_-]/", "", str_replace($de,$para,$string));
     }
-    
-    // return queries strings by url 
-   /* static public function queryString($value = null) 
-    {
-        $queries = filter_input_array(INPUT_GET);        
-        if ($queries) {
-            foreach ($queries as $key => $valueQueries) {
-                $array[] = "$key=$valueQueries";
-            }
-        }
-        return  $value && $queries ? (array_key_exists($value, $queries) ? $queries[$value] : null) : (isset($array) ? implode("&", $array) : null);
-    }*/
 }
-
