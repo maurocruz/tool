047af91195df4b1bda42471a203d852a48a77304
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-09-24 15:22:58 -0300

    styling

--- src/DateTime.php
+++ src/DateTime.php
@@ -2,6 +2,8 @@
 
 namespace Plinct\Tool;
 
+use Exception;
+
 class DateTime
 {
     public $year;
@@ -18,26 +20,32 @@ class DateTime
     
     public function __construct($date, $object = NULL) 
     {
-        $datetime = new \DateTime($date, $object);
-        $this->year = $datetime->format("Y");
-        $this->month = $datetime->format("n");
-        $this->day = $datetime->format("d");
-        $this->hour = $datetime->format("h");
-        $this->minute = $datetime->format("i");
-        $this->second = $datetime->format("s");
-        $this->weekday = $datetime->format("N");
-        $this->literalMonth = self::translateMonth($this->month);
-        $this->literalMonthAbrev = substr(self::translateMonth($this->month),0,3);
-        $this->literalWeekDay = self::translateWeekday($this->weekday);
-        $this->literalWeekDayAbrev = substr(self::translateWeekday($this->weekday),0,3);
+        try {
+            $datetime = new \DateTime($date, $object);
+            $this->year = $datetime->format("Y");
+            $this->month = $datetime->format("n");
+            $this->day = $datetime->format("d");
+            $this->hour = $datetime->format("h");
+            $this->minute = $datetime->format("i");
+            $this->second = $datetime->format("s");
+            $this->weekday = $datetime->format("N");
+            $this->literalMonth = self::translateMonth($this->month);
+            $this->literalMonthAbrev = substr(self::translateMonth($this->month),0,3);
+            $this->literalWeekDay = self::translateWeekday($this->weekday);
+            $this->literalWeekDayAbrev = substr(self::translateWeekday($this->weekday),0,3);
+        } catch (Exception $e) {
+        }
     }
     
     // formata Dia dd/mm weekday
-    public static function formatDateWithWeekday($data)
+    /*public static function formatDateWithWeekday($data)
     {
-        $date = new \DateTime($data);
+        try {
+            $date = new \DateTime($data);
+        } catch (Exception $e) {
+        }
         return $date->format('d').'/'.$date->format('n').' '.self::translateWeekday($date->format('N'));
-    } 
+    } */
     
     // RETORNA UM PERÍODO DE datainicio A datafim
     static function formatDate($start = null, $end = null, $mode = 'NUMERAL') 
@@ -45,10 +53,13 @@ class DateTime
         if ($start === false) {
             return "Data inválida!";
         }
-        
-        $inicio = new \DateTime($start);
-        $fim = $end != null ? new \DateTime($end) : null;
-        
+
+        try {
+            $inicio = new \DateTime($start);
+            $fim = $end != null ? new \DateTime($end) : null;
+        } catch (Exception $e) {
+        }
+
         // calculando a diferença
         $interval = $fim ? $inicio->diff($fim) : null;
         $connective = $interval && $interval->format('%a') > 1 ? 'a' : 'e';
@@ -69,11 +80,14 @@ class DateTime
     }
     
     static function formatDateTime($date){
-        $data = new \DateTime($date);
+        try {
+            $data = new \DateTime($date);
+        } catch (Exception $e) {
+        }
         return date_format($data, "d/m/Y \à\s H\:i\:s");
     }  
     
-    public function getDataUser($id,$field){
+   /*public function getDataUser($id,$field){
         $db = new UsersModel();
         $where = "id='{$id}'";
         $dados_user = $db->selectUser($where);
@@ -82,10 +96,10 @@ class DateTime
         }else{
             return $dados_user[0][$field];
         }
-    }
+    }*/
         
     // RETORNA HORAS NO FORMATO BRASILEIRO
-    static function formatBrazilianTime($time){
+    /*static function formatBrazilianTime($time){
         $hour = substr($time, 0, 2);
         $minutes = substr($time, 3, 2);
         $seconds = substr($time, 6, 2);
@@ -94,11 +108,14 @@ class DateTime
         }else{
             return $hour.'h'.$minutes.'min'.$seconds;
         }
-    }
+    }*/
 
     // RETORNA DATA COMO TEXTO
     public static function getTextualDate($date = NULL, $weekday = NULL) {
-        $data = $date ? new \DateTime($date) : new \DateTime();
+        try {
+            $data = $date ? new \DateTime($date) : new \DateTime();
+        } catch (Exception $e) {
+        }
         // weekday
         $week = $weekday ? self::translateWeekday(date_format($data, 'N')).', ' : NULL;
         // dia
@@ -109,8 +126,9 @@ class DateTime
         return $week.$day." de ".$month." de ".$year;
     }
     
-    public static function translateWeekday($weekday, $abrev = NULL){
-        if($abrev){            
+    public static function translateWeekday($weekday, $abrev = NULL)
+    {
+        if ($abrev) {
             switch ($weekday) { 
                 case "1": return "seg"; 
                 case "2": return "ter";  
@@ -120,7 +138,7 @@ class DateTime
                 case "6": return "sáb"; 
                 case "7": return "dom";
             }
-        }else{
+        } else {
             switch ($weekday) { 
                 case "1": return "segunda"; 
                 case "2": return "terça";  
@@ -131,8 +149,11 @@ class DateTime
                 case "7": return "domingo";
             }
         }
+        return null;
     }
-    public static function translateMonth($month){
+
+    public static function translateMonth($month)
+    {
         switch ($month) { 
             case "1": return "janeiro"; 
             case "2": return "fevereiro";  
@@ -147,8 +168,11 @@ class DateTime
             case "11": return "novembro";
             case "12": return "dezembro";
         }
+        return null;
     }
-    public static function formateCompleteDateTimeUTC($date){
+
+    public static function formateCompleteDateTimeUTC($date)
+    {
         $parse = date_parse_from_format("Y-m-d H:i:s", $date);
         return date("c", mktime($parse['hour'], $parse['minute'], $parse['second'], $parse['month'], $parse['day'], $parse['year']));
     }
@@ -157,8 +181,7 @@ class DateTime
     {
         $replace = str_replace(":", "%s", $time);
         $string = sprintf($replace, "h", "min");
-        $wsec = substr($string, 0, -5);
-        return $wsec;
+        return substr($string, 0, -5);
     }
 }
 
