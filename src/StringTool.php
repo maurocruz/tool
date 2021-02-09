9b279cbd3fe913020cd8af48743217162dcf77ab
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-09 14:54:54 -0300

    Added function translate words

--- src/StringTool.php
+++ src/StringTool.php
@@ -3,18 +3,31 @@
 namespace Plinct\Tool;
 
 class StringTool 
-{            
+{
     /**
      * REMOVE ACENTOS E ESPAÇOS EM TEXTO
      * @param string $string
      * @return string
      */
-    public static function removeAccentsAndSpaces($string)
+    public static function removeAccentsAndSpaces(string $string): string
     {
         $de = array('Á','Í','Ó','Ú','É','Ä','Ï','Ö','Ü','Ë','À','Ì','Ò','Ù','È','Ã','Õ','Â','Î','Ô','Û','Ê','á','í','ó','ú','é','ä','ï','ö','ü','ë','à','ì','ò','ù','è','ã','õ','â','î','ô','û','ê','Ç','ç',' ');
         
         $para = array('A','I','O','U','E','A','I','O','U','E','A','I','O','U','E','A','O','A','I','O','U','E','a','i','o','u','e','a','i','o','u','e','a','i','o','u','e','a','o','a','i','o','u','e','C','c','');
         
-        return preg_replace("/[^\.\/a-zA-Z0-9_-]/", "", str_replace($de,$para,$string));
+        return preg_replace("/[^.\/a-zA-Z0-9_-]/", "", str_replace($de,$para,$string));
+    }
+
+    public static function translateWordsSeparatedByDelimiter(string $delimiter, string $string = null): ?string
+    {
+        if($string) {
+            $array = explode($delimiter, $string);
+            foreach ($array as $value) {
+                $translate = _(trim($value));
+                $newArray[] = $translate;
+            }
+            return implode($delimiter . " ", $newArray);
+        }
+        return null;
     }
 }
