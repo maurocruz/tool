db7fa892b0f00895296e4f4cc2b19cad38fdcb03
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-03-23 16:50:52 -0300

    Created methods for post, put and delete

--- src/Curl.php
+++ src/Curl.php
@@ -1,24 +1,104 @@
 <?php
 namespace Plinct\Tool;
 
+/**
+ * Class Curl
+ * @package Plinct\Tool
+ */
 class Curl {
-    private $basePath;
+    /**
+     * @var string
+     */
+    private string $basePath;
 
+    /**
+     * Curl constructor.
+     * @param $basePath
+     */
     public function __construct($basePath) {
         $this->basePath = $basePath;
     }
 
+    /**
+     * @param string $relativeUrl
+     * @return string
+     */
+    private function getUrl(string $relativeUrl): string {
+        return $this->basePath . $relativeUrl;
+    }
+
+    /**
+     * @param string $relativeUrl
+     * @param array|null $params
+     * @return bool|string
+     */
     public function get(string $relativeUrl, array $params = null) {
-        $url = $this->basePath . $relativeUrl . "?" . http_build_query($params);
-        $handle = curl_init($url);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $urlWithQueries = $relativeUrl . "?" . http_build_query($params);
+        return $this->request("get", $urlWithQueries);
+    }
+
+    /**
+     * @param string $relativeUrl
+     * @param array $params
+     * @param string $token
+     * @return bool|string
+     */
+    public function post(string $relativeUrl, array $params, string $token) {
+        return $this->request("post", $relativeUrl, $params, $token);
+    }
+
+    /**
+     * @param string $relativeUrl
+     * @param array $params
+     * @param string $token
+     * @return bool|string
+     */
+    public function put(string $relativeUrl, array $params, string $token) {
+        return $this->request("put", $relativeUrl, $params, $token);
+    }
+
+    /**
+     * @param string $relativeUrl
+     * @param array $params
+     * @param string $token
+     * @return bool|string
+     */
+    public function delete(string $relativeUrl, array $params, string $token) {
+        return $this->request("delete", $relativeUrl, $params, $token);
+    }
+
+    /**
+     * @param string $type
+     * @param string $relativeUrl
+     * @param array|null $params
+     * @param string|null $token
+     * @return bool|string
+     */
+    private function request(string $type, string $relativeUrl, array $params = null, string $token = null) {
+        $method = strtoupper($type);
+        // CURL INIT
+        $handle = curl_init($this->getUrl($relativeUrl));
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        // HEADERS
+        $headers[] = "Content-Type: application/json";
+        if ($token) $headers[] = "Authorization: Bearer $token";
+        curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+        // METHOD
+        curl_setopt($handle, CURLOPT_CUSTOMREQUEST, $method);
+        // PARAMS
+        if ($params) curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($params, JSON_UNESCAPED_SLASHES));
+        // disable for production
+        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($handle, CURLOPT_PROXY_SSL_VERIFYPEER, false);
+        // EXECUTE
         $exec = curl_exec($handle);
         curl_close($handle);
         return $exec;
     }
 
+    /**
+     * @return array
+     */
     public function getImageData(): array {
         $handle = curl_init($this->basePath);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
