a25f9471bb833b51e05a12c87ae810c252b4b6c1
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-16 11:22:10 -0300

    Fixed error on set schema protocol

--- src/Image.php
+++ src/Image.php
@@ -19,7 +19,7 @@ class Image {
     private $height;
     private $ratio;
     private $fileSize = null;
-    private $src = "https://pirenopolis.tur.br/App/static/cms/images/noImage.jpg";
+    protected $src = "https://pirenopolis.tur.br/App/static/cms/images/noImage.jpg";
     private $docRoot;
     protected $requestUri;
     private $httpHost;
@@ -71,7 +71,7 @@ class Image {
             $this->src = $this->source;
         } else {
             if (!$this->scheme) {
-                $protocol = filter_input(INPUT_SERVER, 'HTTPS') != 'off' ? "https" : "http";
+                $protocol = filter_input(INPUT_SERVER, 'HTTPS') && filter_input(INPUT_SERVER, 'HTTPS') != 'off' ? "https" : "http";
                 $this->src = str_replace($this->docRoot, $protocol . "://" . $this->httpHost, $this->pathFile);
             } else {
                 $this->src = $this->source;
