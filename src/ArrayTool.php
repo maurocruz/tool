90c9243256bfd384b02311f0a705228575af5e3a
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-18 13:11:50 -0300

    ArrayTool.php - fix error

--- src/ArrayTool.php
+++ src/ArrayTool.php
@@ -6,14 +6,8 @@ class ArrayTool {
     public static function searchByValue(array $array, string $valueName, string $propertyName = null) {
         if ($array) {
             foreach ($array as $value) {
-                if ($value == $valueName) {
-                    return $array;
-                } elseif (is_array($value)) {
-                    $data = self::searchByValue($value, $valueName);
-                    if ($data && $propertyName) {
-                        return $data[$propertyName] ?? null;
-                    }
-                    return $data;
+                if (array_search($valueName, $value)) {
+                    return $value;
                 }
             }
         }
