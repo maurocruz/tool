a7e1e8621b9f094b05ea3951f804071e102921a3
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-08-04 14:57:28 -0300

    Image.php - added in construct setExtension

--- src/Image/Image.php
+++ src/Image/Image.php
@@ -1,6 +1,8 @@
 <?php
 namespace Plinct\Tool\Image;
 
+use Exception;
+
 class Image extends Thumbnail implements ImageTransformInterface {
 
     /**
@@ -13,12 +15,15 @@ class Image extends Thumbnail implements ImageTransformInterface {
         $posLastSeparator = strrpos($this->requestUri, "/");
         $requestUri = substr($this->requestUri, 0, ($posLastSeparator + 1));
         $this->source = (substr($source,0,1) != "/" && substr($source,0,4) != "http" ? $requestUri . $source : $source) ?? $this->src;
+        // extension
+        $this->setExtension();
     }
 
     /**
      * @param $width
      * @param null $height
      * @return ImageTransformInterface
+     * @throws Exception
      */
     public function resize($width, $height = null): ImageTransformInterface {
         if (!$this->width) parent::setSizes();
@@ -35,9 +40,10 @@ class Image extends Thumbnail implements ImageTransformInterface {
     /**
      * @param $width
      * @param null $height
-     * @return mixed
+     * @return string
+     * @throws Exception
      */
-    public function thumbnail($width, $height = null) {
+    public function thumbnail($width, $height = null): string {
         if (!$this->width) parent::setSizes();
         return parent::getThumbnail($width, $height);
     }
@@ -52,21 +58,27 @@ class Image extends Thumbnail implements ImageTransformInterface {
 
     /**
      * WIDTH
-     * @return mixed
+     * @return int|null
+     * @throws Exception
      */
-    public function getWidth() {
+    public function getWidth(): ?int {
         if (!$this->width) $this->setSizes();
         return $this->width;
     }
 
     /**
      * HEIGHT
-     * @return mixed
+     * @return int|null
+     * @throws Exception
      */
-    public function getHeight() {
+    public function getHeight(): ?int {
         if (!$this->height) $this->setSizes();
         return $this->height;
     }
+
+    /**
+     * @throws Exception
+     */
     public function getFileSize(): ?int {
         if (!$this->fileSize) $this->setSizes();
         return $this->fileSize;
@@ -74,20 +86,32 @@ class Image extends Thumbnail implements ImageTransformInterface {
     public function getSource(): string {
         return $this->source;
     }
+
+    /**
+     * @throws Exception
+     */
     public function getSrc(): string {
         if (!$this->src) $this->setSrc();
         return $this->src;
     }
+
+    /**
+     * @throws Exception
+     */
     public function getNewHeight(): int {
         if (!$this->newHeight) parent::setSizes();
         return $this->newHeight;
     }
+
+    /**
+     * @throws Exception
+     */
     public function getNewWidth(): int {
         if (!$this->newWidth) parent::setSizes();
         return $this->newWidth;
     }
 
-    public function getEncodingFormat(){
+    public function getEncodingFormat(): string {
         return $this->encodingFormat;
     }
 }
