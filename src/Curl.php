f0443a4932441feda29db6e9927cef6c259afc2b
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-08-26 11:49:42 -0300

    Added PHP doc

--- src/Curl.php
+++ src/Curl.php
@@ -1,14 +1,19 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Plinct\Tool;
 
 use Exception;
+
 use SimpleXMLElement;
 
 /**
  * Class Curl
  * @package Plinct\Tool
  */
-class Curl {
+class Curl
+{
     /**
      * @var string
      */
@@ -18,7 +23,8 @@ class Curl {
      * Curl constructor.
      * @param $basePath
      */
-    public function __construct($basePath) {
+    public function __construct($basePath)
+    {
         $this->basePath = $basePath;
     }
 
@@ -26,7 +32,8 @@ class Curl {
      * @param string $relativeUrl
      * @return string
      */
-    private function getUrl(string $relativeUrl): string {
+    private function getUrl(string $relativeUrl): string
+    {
         return $this->basePath . (strpos($this->basePath, '/', -1) === false ? "/" : null) . $relativeUrl;
     }
 
@@ -35,7 +42,8 @@ class Curl {
      * @param array|null $params
      * @return bool|string
      */
-    public function get(string $relativeUrl, array $params = null) {
+    public function get(string $relativeUrl, array $params = null)
+    {
         $urlWithQueries = $relativeUrl . "?" . http_build_query($params);
         return $this->request("get", $urlWithQueries);
     }
@@ -46,7 +54,8 @@ class Curl {
      * @param string|null $token
      * @return bool|string
      */
-    public function post(string $relativeUrl, array $params, string $token = null) {
+    public function post(string $relativeUrl, array $params, string $token = null)
+    {
         return $this->request("post", $relativeUrl, $params, $token);
     }
 
@@ -56,7 +65,8 @@ class Curl {
      * @param string $token
      * @return bool|string
      */
-    public function put(string $relativeUrl, array $params, string $token) {
+    public function put(string $relativeUrl, array $params, string $token)
+    {
         return $this->request("put", $relativeUrl, $params, $token);
     }
 
@@ -66,7 +76,8 @@ class Curl {
      * @param string $token
      * @return bool|string
      */
-    public function delete(string $relativeUrl, array $params, string $token) {
+    public function delete(string $relativeUrl, array $params, string $token)
+    {
         return $this->request("delete", $relativeUrl, $params, $token);
     }
 
@@ -77,25 +88,35 @@ class Curl {
      * @param string|null $token
      * @return bool|string
      */
-    private function request(string $type, string $relativeUrl, array $params = null, string $token = null) {
+    private function request(string $type, string $relativeUrl, array $params = null, string $token = null)
+    {
         $method = strtoupper($type);
+
         // CURL INIT
         $handle = curl_init($this->getUrl($relativeUrl));
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($handle, CURLOPT_MAXREDIRS, 3);
+
         // HEADERS
         $headers[] = "Content-Type: application/json";
         if ($token) $headers[] = "Authorization: Bearer $token";
         curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+
         // METHOD
         curl_setopt($handle, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+
         // PARAMS
         if ($params) curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($params, JSON_UNESCAPED_SLASHES));
+
         // disable for production
         if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == "::1") {
             curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
         }
+
         // EXECUTE
         $exec = curl_exec($handle);
+
         if ($exec === false) {
             $response = curl_error($handle);
         } else {
@@ -109,7 +130,8 @@ class Curl {
      * @return array|false
      * @throws Exception
      */
-    public function getImageData(): array {
+    public function getImageData(): array
+    {
         $validate = false;
         $type = null;
         $width = null;
@@ -119,7 +141,11 @@ class Curl {
         // HANDLE
         $handle = curl_init($this->basePath);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($handle, CURLOPT_MAXREDIRS, 3);
+
         $data = curl_exec($handle);
+
         // CHECK IF RESPONSE IS 200
         $responseCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
         $contentType = curl_getinfo($handle, CURLINFO_CONTENT_TYPE);
@@ -148,6 +174,7 @@ class Curl {
         }
         // CLOSE
         curl_close($handle);
+
         // ARRAY RESPONSE
         $response['validate'] = $validate;
         $response['fileSize'] = $fileSize;
@@ -157,18 +184,27 @@ class Curl {
             $type,
             'mime'=>$contentType
         ];
+
         // RESPONSE
         return $response;
     }
 
-    public static function remote_file_exists($filename): bool {
+    /**
+     * @param $filename
+     * @return bool
+     */
+    public static function remote_file_exists($filename): bool
+    {
         // Initialize cURL
         $ch = curl_init($filename);
         curl_setopt($ch, CURLOPT_NOBODY, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
         curl_exec($ch);
         $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
         curl_close($ch);
+
         // Check the response code
         if($responseCode == 200){
             return true;
@@ -177,12 +213,22 @@ class Curl {
         }
     }
 
-    public static function getUrlContents(string $URL) {
+    /**
+     * @param string $URL
+     * @return bool|string
+     */
+    public static function getUrlContents(string $URL)
+    {
         $handle = curl_init($URL);
+
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($handle, CURLOPT_MAXREDIRS, 3);
+
         if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == "::1") {
             curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
         }
+
         $data = curl_exec($handle);
         curl_close($handle);
         return $data;
