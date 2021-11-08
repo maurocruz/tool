cd465486f82ad48b34e83510f27b3767f914b8e5
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-11-08 15:39:40 -0300

    Fix error and replace invalidated url string

--- src/StructuredData/v1/Type/ImageObject.php
+++ src/StructuredData/v1/Type/ImageObject.php
@@ -16,6 +16,16 @@ class ImageObject extends StructuredDataTypeAbstract
         parent::includeProperty('license');
         parent::includeProperty('acquireLicensePage');
 
+        if (isset($this->newData['@type'])) {
+            $this->newData['contentUrl'] = str_replace(" ", "%20", $this->newData['url']);
+            $this->newData['url'] = str_replace(" ", "%20", $this->newData['url']);
+        } else {
+            foreach ($this->newData as $key => $value) {
+                $this->newData[$key]['contentUrl'] = str_replace(" ","%20",$value['url']);
+                $this->newData[$key]['url'] = str_replace(" ","%20",$value['url']);
+            }
+        }
+
         return $this->newData;
     }
 }
