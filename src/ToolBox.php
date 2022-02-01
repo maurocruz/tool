f50f0e11b0c61d3c1dd73cd4bc3103a6582797ba
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-02-01 12:16:34 -0300

    include get representative image of page in tool box

--- src/ToolBox.php
+++ src/ToolBox.php
@@ -25,4 +25,23 @@ class ToolBox
     {
         return new Curl();
     }
+
+
+    /**
+     * @param $data
+     * @param string $mode
+     * @return null
+     */
+    public static function getRepresentativeImageOfPage($data, string $mode = "string")
+    {
+        if ($data) {
+            foreach ($data as $valueImage) {
+                if (isset($valueImage['representativeOfPage']) && $valueImage['representativeOfPage'] == true) {
+                    return $mode == "string" ? $valueImage['contentUrl'] : $valueImage;
+                }
+            }
+            return $mode == "string" ? $data[0]['contentUrl'] : $data[0];
+        }
+        return null;
+    }
 }
