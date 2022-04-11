eb5215507843376adb8938eb646cb71d466bbaa7
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-04-11 10:57:42 -0300

    Included geo property with GeoCoordinates data

--- src/StructuredData/v1/Type/Place.php
+++ src/StructuredData/v1/Type/Place.php
@@ -6,16 +6,22 @@ namespace Plinct\Tool\StructuredData\v1\Type;
 
 class Place extends StructuredDataTypeAbstract
 {
-    /**
-     * @return array
-     */
-    public function parse(): array
-    {
-        parent::includeProperty('latitude');
-        parent::includeProperty('longitude');
-
-        $this->newData['address'] = $this->data['address'] ? (new PostalAddress($this->data['address']))->parse() : null;
-
-        return $this->newData;
+  /**
+   * @return array
+   */
+  public function parse(): array
+  {
+	  // GEO COORDINATES
+    if ($this->data['longitude'] && $this->data['latitude']) {
+			$this->newData['geo'] = [
+				'@type'=>'GeoCoordinates',
+				'latitude'=>(float)$this->data['latitude'],
+				'longitude'=>(float)$this->data['longitude']
+			];
     }
+		// ADDRESS
+    $this->newData['address'] = $this->data['address'] ? (new PostalAddress($this->data['address']))->parse() : null;
+		// NEW DATA
+    return $this->newData;
+  }
 }
