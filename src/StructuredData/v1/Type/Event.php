4efc243e3ae7a79fd9da98629d4334e002122f7a
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-04-11 11:19:29 -0300

    Fix image in event type and strip tags in description property

--- src/StructuredData/v1/Type/Event.php
+++ src/StructuredData/v1/Type/Event.php
@@ -6,21 +6,25 @@ namespace Plinct\Tool\StructuredData\v1\Type;
 
 class Event extends StructuredDataTypeAbstract
 {
-    /**
-     * @return array
-     */
-    public function parse(): array
-    {
-        $startDate = substr($this->data['startDate'],11) == "00:00:00" ? substr($this->data['startDate'],0,10) : $this->data['startDate'];
-        $endDate = substr($this->data['endDate'],11) == "00:00:00" ? substr($this->data['endDate'],0,10) : $this->data['endDate'];
-
-        $this->newData['startDate'] = $startDate;
-        $this->newData['endDate'] = $endDate;
-
-        if (isset($this->data['location'])) $this->newData['location'] = (new Place($this->data['location']))->parse();
-
-        if (isset($this->data['image'])) $this->newData['image'] = (new ImageObject($this->data['image']))->parse();
-
-        return $this->newData;
-    }
+  /**
+   * @return array
+   */
+  public function parse(): array
+  {
+		// DATES
+    $startDate = substr($this->data['startDate'],11) == "00:00:00" ? substr($this->data['startDate'],0,10) : $this->data['startDate'];
+    $endDate = substr($this->data['endDate'],11) == "00:00:00" ? substr($this->data['endDate'],0,10) : $this->data['endDate'];
+    $this->newData['startDate'] = $startDate;
+    $this->newData['endDate'] = $endDate;
+		// LOCATION
+    if (isset($this->data['location'])) $this->newData['location'] = (new Place($this->data['location']))->parse();
+		// IMAGE
+	  if (isset($this->data['image']) && !empty($this->data['image'])) {
+			foreach ($this->data['image'] as $value) {
+				$this->newData['image'][] = $value['contentUrl'];
+			}
+	  }
+		// RETURN
+    return $this->newData;
+  }
 }
