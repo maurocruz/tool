0dcd60235ddc672eb239fbef5feca0f836f26e4c
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-08-23 12:26:11 -0300

    preparando para versão 2

--- src/Curl/v1/Curl.php
+++ src/Curl/v1/Curl.php
@@ -6,10 +6,6 @@ namespace Plinct\Tool\Curl\v1;
 
 class Curl
 {
-	/**
-	 * @var string
-	 */
-	private string $method;
   /**
    * @var false|resource
    */
@@ -22,12 +18,17 @@ class Curl
    * @var bool|string
    */
   private $exec = false;
+	/**
+	 * @var string
+	 */
+	private string $url;
 
   /**
    *
    */
   public function __construct(string $url = '')
   {
+		$this->url = $url;
     $this->handle = curl_init($url);
     curl_setopt($this->handle, CURLOPT_HEADER, false);
   }
@@ -48,6 +49,7 @@ class Curl
    */
   public function setUrl(string $url): Curl
   {
+	  $this->url = $url;
     curl_setopt($this->handle, CURLOPT_URL, $url);
     return $this;
   }
@@ -59,18 +61,6 @@ class Curl
   {
     if (!$this->exec) $this->execute();
     return curl_getinfo($this->handle);
-  }
-
-  /**
-   * @param string $method
-   * @return $this
-   */
-  public function method(string $method): Curl
-  {
-		$this->method = strtoupper($method);
-		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, strtoupper($method));
-
-    return $this;
   }
 
   /**
@@ -102,28 +92,22 @@ class Curl
     return $this;
   }
 
-  /**
-   * @param array $params
-   * @return $this
-   */
-  public function params(array $params): Curl
-  {
-    if ($params) {
-			if ($this->method == "POST" || $this->method == "PUT" || $this->method == "DELETE") {
-				curl_setopt($this->handle, CURLOPT_POSTFIELDS, http_build_query($params));
-			} else {
-				curl_setopt($this->handle, CURLOPT_POSTFIELDS, json_encode($params, JSON_UNESCAPED_SLASHES));
-			}
-    }
-    return $this;
-  }
+	/**
+	 * @param array $params
+	 * @return $this
+	 */
+	public function get(array $params): Curl {
+		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, "GET");
+		curl_setopt($this->handle, CURLOPT_URL, $this->url."?".http_build_query($params));
+		return $this;
+	}
 
 	/**
 	 * @param $data
 	 * @return $this
 	 */
-	public function post($data): Curl
-	{
+	public function post($data): Curl {
 		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($this->handle, CURLOPT_POSTFIELDS, http_build_query($data));
 		curl_setopt($this->handle, CURLOPT_POST, true);
@@ -132,24 +116,24 @@ class Curl
 
 	/**
 	 * @param $data
-	 * @return void
+	 * @return $this
 	 */
-	public function put($data)
-	{
+	public function put($data): Curl {
 		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, "PUT");
 		curl_setopt($this->handle, CURLOPT_POSTFIELDS, http_build_query($data));
+		return $this;
 	}
 
 	/**
-  * @param $data
+  * @param $params
   * @return $this
   */
-	public function delete($data): Curl
+	public function delete($params): Curl
 	{
 		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, "DELETE");
-		curl_setopt($this->handle, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($this->handle, CURLOPT_URL, $this->url."?".http_build_query($params));
 		return $this;
 	}
 
