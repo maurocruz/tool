ea234de2057dd4a4be642c554b4fb1dd3814ccbc
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2023-08-09 13:41:32 -0300

    Fix date format in date for events

--- src/StructuredData/v1/Type/Event.php
+++ src/StructuredData/v1/Type/Event.php
@@ -4,18 +4,37 @@ declare(strict_types=1);
 
 namespace Plinct\Tool\StructuredData\v1\Type;
 
+use DateTime;
+use DateTimeInterface;
+use Exception;
+
 class Event extends StructuredDataTypeAbstract
 {
-  /**
-   * @return array
-   */
+	/**
+	 * @return array
+	 * @throws Exception
+	 */
   public function parse(): array
   {
 		// DATES
-    $startDate = substr($this->data['startDate'],11) == "00:00:00" ? substr($this->data['startDate'],0,10) : $this->data['startDate'];
-    $endDate = substr($this->data['endDate'],11) == "00:00:00" ? substr($this->data['endDate'],0,10) : $this->data['endDate'];
-    $this->newData['startDate'] = $startDate;
+	  // start date format
+	  if (substr($this->data['startDate'],11) === "00:00:00") {
+		  $startDate = substr($this->data['startDate'],0,10);
+	  } else {
+		  $sd = new DateTime($this->data['startDate']);
+			$startDate = $sd->format(DateTimeInterface::ATOM);
+	  }
+		// end date format
+		if (substr($this->data['startDate'],11) === "00:00:00" ) {
+			$endDate = substr($this->data['endDate'],0,10);
+		} else {
+			$ed = new DateTime($this->data['endDate']);
+			$endDate = $ed->format(DateTimeInterface::ATOM);
+		}
+
+	  $this->newData['startDate'] = $startDate;
     $this->newData['endDate'] = $endDate;
+
 		// LOCATION
     if (isset($this->data['location'])) $this->newData['location'] = (new Place($this->data['location']))->parse();
 		// IMAGE
