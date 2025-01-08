e69ea071551920c16b70097b215a3b81a4d0da9a
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2025-01-08 16:02:41 -0300

    added get idthing and get type

--- src/TypeBuilder.php
+++ src/TypeBuilder.php
@@ -1,5 +1,4 @@
 <?php
-declare(strict_types=1);
 namespace Plinct\Tool;
 
 class TypeBuilder
@@ -8,25 +7,62 @@ class TypeBuilder
 	 * @var array
 	 */
 	private array $data;
+	/**
+	 * @var string
+	 */
 	private string $idname;
+	/**
+	 * @var mixed
+	 */
 	private $identifier;
+	/**
+	 * @var string|mixed|null
+	 */
+	private string $type;
 
+	/**
+	 * @param array $value
+	 */
 	public function __construct(array $value) {
-		$type = $value['@type'] ?? null;
-		$this->idname = "id".lcfirst($type);
+		$this->type = $value['@type'] ?? null;
+		$this->idname = "id".lcfirst($this->type);
 		$this->data = $value;
 		$this->identifier = $value['identifier'];
 	}
 
+	/**
+	 * @return int|null
+	 */
 	public function getId(): ?int
 	{
 		return (int) $this->getPropertyValue($this->idname);
 	}
 
+	public function getIdthing(): ?string
+	{
+		return $this->getPropertyValue('idthing');
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getType(): string
+	{
+		return $this->type;
+	}
+
+	/**
+	 * @param string $property
+	 * @return mixed|null
+	 */
 	public function getValue(string $property) {
 		return $this->data[$property] ?? null;
 	}
-	
+
+	/**
+	 * @param string $property
+	 * @return false|mixed
+	 */
 	public function getPropertyValue(string $property)	{
 		foreach ($this->identifier as $propertyValue) {
 			if ($propertyValue['name'] === $property) {
